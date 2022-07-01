de5c1dc87074b4de64225c99660ee814ac46c4d0
xkstksfpahs <heis2514@gmail.com>
2022-07-01 20:02:39 +0900

    22/07/01 작업
    터렛 애니메이션 넣는중
    터렛에 추가할 해치 추가
    터렛 생성 전 기본 idle동작 추가

---
+++ Assets/PlaceSearch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSearch : MonoBehaviour
+{
+
+    public bool CI = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(CI);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CI);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Turret"))
+            CI = false;
+        Debug.Log(CI);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        CI = true;
+        Debug.Log(CI);
+    }
+}
